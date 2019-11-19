edcb72512dca4685549a30ae942014f661c72cf7
Karol Mendza <karolmendza@gmail.com>
2019-11-19 23:11:57 +0100

    Initial commit

---
+++ src/main/java/msg/webstore/salesandinventory/MsgWebstoreSalesAndInventoryApplication.java
@@ -0,0 +1,13 @@
+package msg.webstore.salesandinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgWebstoreSalesAndInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsgWebstoreSalesAndInventoryApplication.class, args);
+    }
+
+}
